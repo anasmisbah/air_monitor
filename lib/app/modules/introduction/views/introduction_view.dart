50d0e09f28bf6f6662e7483908a73eb935122cd7
anas misbah <anas.misbahuddin4869@gmail.com>
2023-06-13 20:16:30 +0800

    intro screen

--- lib/app/modules/introduction/views/introduction_view.dart
+++ lib/app/modules/introduction/views/introduction_view.dart
@@ -1,4 +1,7 @@
+import 'package:air_monitor/app/consntans/color.dart';
+import 'package:air_monitor/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
@@ -9,14 +12,79 @@ class IntroductionView extends GetView<IntroductionController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('IntroductionView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'IntroductionView is working',
-          style: TextStyle(fontSize: 20),
+      body: Container(
+        width: Get.width,
+        height: Get.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/bg_splash.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Selamat Datang di",
+                      style: TextStyle(
+                        fontSize: 28.w,
+                        fontWeight: FontWeight.w600,
+                        color: AppColor.surface2,
+                        letterSpacing: 0.05,
+                      ),
+                    ),
+                    Text(
+                      "aplikasi monitoring kadar polusi udara berbasis Internet of Things yang terkoneksi dengan perangkat anda",
+                      style: TextStyle(
+                        fontSize: 14.w,
+                        color: AppColor.white,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 66,
+                ),
+                Image.asset(
+                  "assets/images/ilustration1.png",
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minWidth: Get.width,
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Get.offNamed(Routes.HOME);
+                    },
+                    child: Text(
+                      "Mulai",
+                      style: TextStyle(
+                        fontSize: 16.w,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColor.main,
+                      padding: EdgeInsets.symmetric(vertical: 12.w),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
@@ -6,11 +7,14 @@ import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: Routes.SPLASH_SCREEN,
-      getPages: AppPages.routes,
-      theme: ThemeData(fontFamily: 'Poppins'),
+    ScreenUtilInit(
+      designSize: Size(375, 812),
+      builder: (context, child) => GetMaterialApp(
+        title: "Application",
+        initialRoute: Routes.SPLASH_SCREEN,
+        getPages: AppPages.routes,
+        theme: ThemeData(fontFamily: 'Poppins'),
+      ),
     ),
   );
 }
