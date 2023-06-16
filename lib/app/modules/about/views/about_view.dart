dd53337426624217265a320638641b143110b011
Anas Misbah <anas.misbahuddin4869@gmail.com>
2023-06-17 07:23:02 +0800

    about

--- lib/app/modules/about/views/about_view.dart
+++ lib/app/modules/about/views/about_view.dart
@@ -1,4 +1,6 @@
+import 'package:air_monitor/app/consntans/color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
@@ -10,14 +12,76 @@ class AboutView extends GetView<AboutController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AboutView'),
+        title: const Text('Tentang Aplikasi'),
         centerTitle: true,
+        backgroundColor: AppColor.main2,
+        elevation: 0,
       ),
-      body: const Center(
-        child: Text(
-          'AboutView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(4.w),
+            width: Get.width,
+            decoration: BoxDecoration(
+              color: AppColor.main2,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: AppColor.main2,
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: AppColor.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Image.asset("assets/images/logo.png"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "\u2981 Versi : 1.0.0",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: AppColor.text,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          "\u2981 Build : 1.0.0",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: AppColor.text,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Divider(
+                      color: AppColor.main3,
+                    ),
+                    Text(
+                      "aplikasi monitoring kadar polusi udara berbasis Internet of Things yang terkoneksi dengan perangkat anda (NB: Bisa diedit atau ditambahkan lagi nanti)",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16.w,
+                        color: AppColor.text,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
