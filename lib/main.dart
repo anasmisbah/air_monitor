8f94e19b90fb1ab14a3ec2d66b4e3547b459459d
anas misbah <anas.misbahuddin4869@gmail.com>
2023-06-19 09:02:49 +0800

    work test

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:workmanager/workmanager.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
+  Workmanager().initialize(
+    callbackDispatcher, // The top level function, aka callbackDispatcher
+    isInDebugMode: true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
+  );
+  Workmanager().registerOneOffTask("task-identifier", "simpleTask");
   runApp(
     ScreenUtilInit(
       designSize: Size(375, 812),
@@ -17,4 +23,12 @@ void main() {
       ),
     ),
   );
+}
+
+@pragma('vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) {
+    print("Native called background task: $task"); //simpleTask will be emitted here.
+    return Future.value(true);
+  });
 }
